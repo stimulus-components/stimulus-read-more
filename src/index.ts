b908ebd962d7ba3c45ec24dba598a7682ad60c8e
Guillaume Briday <guillaumebriday@gmail.com>
2024-03-21 22:48:59 -0400

    Updating config and move package to organization

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
+export default class ReadMore extends Controller {
   open: boolean
   contentTarget: HTMLElement
   moreTextValue: string
   lessTextValue: string
 
-  static targets: string[] = ['content']
+  static targets: string[] = ["content"]
   static values = {
     moreText: String,
-    lessText: String
+    lessText: String,
   }
 
-  connect (): void {
+  connect(): void {
     this.open = false
   }
 
-  toggle (event: Event): void {
+  toggle(event: Event): void {
     this.open === false ? this.show(event) : this.hide(event)
   }
 
-  show (event: Event): void {
+  show(event: Event): void {
     this.open = true
 
     const target = event.target as HTMLElement
     target.innerHTML = this.lessTextValue
-    this.contentTarget.style.setProperty('--read-more-line-clamp', "'unset'")
+    this.contentTarget.style.setProperty("--read-more-line-clamp", "'unset'")
   }
 
-  hide (event: Event): void {
+  hide(event: Event): void {
     this.open = false
 
     const target = event.target as HTMLElement
     target.innerHTML = this.moreTextValue
-    this.contentTarget.style.removeProperty('--read-more-line-clamp')
+    this.contentTarget.style.removeProperty("--read-more-line-clamp")
   }
 }
